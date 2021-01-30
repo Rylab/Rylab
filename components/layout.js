42ed472e252a312ab7a7a38ca22e2917c7bbe31a
Ryan LaBarre <ryan@urbansitter.com>
2021-01-30 14:57:39 -0800

    added images and core site-specific CSS, meta, etc

--- components/layout.js
+++ components/layout.js
@@ -1,22 +1,32 @@
 import Head from 'next/head'
 import styles from './layout.module.css'
-import Link from 'next/link'
 
-const name = 'Rylab'
-export const siteTitle = 'RyLaB'
+export const siteTitle = 'RyLaB';
 
-export default function Layout({ children, home }) {
+export default function Layout({ children }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Rylab.js"
-        />
-        <meta name="og:title" content={siteTitle} />
+        <meta charsSet="utf-8" />
+        <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+        <meta name="author" content='Ryan "Rylab" D. LaBarre' />
+        <meta name="language" content="en" />
+        <meta name="robots" content="index, follow" />
+        <meta name="theme-color" content="#222" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta property="og:site_name" content="Rylab.com" />
+        <meta property="og:title" content="RyLaB" />
+        <link rel="icon" type="image/png" href="https://rylab.com/img/bsd_introvert.png" />
       </Head>
       <main>{children}</main>
+      <div id="footer">
+        <p className="small light">
+          <a href="mailto:0@rylab.com">1@rylab.com</a>
+          &nbsp;&middot;&nbsp;
+          <a alt="Content license URL alias for: CC BY-NC-SA 4.0" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" title="Creative Commons BY-NC-SA 4.0 as of <?php echo date(DATE_W3C); ?>">
+            some rights reserved</a>
+        </p>
+      </div>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,91 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 
+const links = [{
+  alias: 'facebook',
+  name: 'Facebook',
+  title: 'Fantastically Friendly Rylab',
+  url: 'https://facebook.com/rylab',
+},{
+  alias: 'github',
+  name: 'GitHub',
+  title: 'Open Source Rylab',
+  url: 'https://github.com/rylab',
+},{
+  alias: 'instagram',
+  name: 'Instagram',
+  title: 'Photographically Social Rylab',
+  url: 'https://www.instagram.com/rylab',
+},{
+  alias: 'linkedin',
+  name: 'LinkedIn',
+  title: 'Super Professional Rylab',
+  url: 'http://lnkd.in/bkDM4Bc',
+},{
+  alias: 'medium-profile',
+  name: 'Medium',
+  title: 'Poetic Author @rylab',
+  url: 'https://medium.com/@rylab',
+},{
+  alias: 'stackoverflow',
+  name: 'StackOverflow',
+  title: 'Pragmatic Programmer @rylab',
+  url: 'https://stackoverflow.com/users/1035173/ryan-lab',
+},{
+  alias: 'twitter',
+  name: 'Twitter',
+  title: 'Tweety @rylab',
+  url: 'https://twitter.com/rylab',
+}];
+
 export default function Index() {
   return (
     <Layout index>
       <Head>
-        <title>{ siteTitle }</title>
+        <link rel="canonical" href="https://rylab.com" />
+        <title>{ siteTitle } :: Home</title>
+        <meta name="og:title" content="{siteTitle} :: Home" />
+        <meta name="description" content="Welcome to Rylab, digital home of Ryan LaBarre" />
+        <meta property="og:description" content="Digital Home of Ryan LaBarre" />
+        <meta property="og:image" content="https://rylab.com/img/bsd_extrovert.png" />
+        <meta property="og:url" content="https://rylab.com" />
       </Head>
-      <section>
-      </section>
+      <div id="header" className="hoverdots">
+        <img className="rylab"
+          src="/img/rylab_extrovert.png"
+          alt="A very pretty building in San Francisco. &copy; Ryan D LaBarre"
+          title="Pretty building in San Francisco. Rylab does not live here (but has been here)" />
+      </div>
+      <div id="rylab">(: hello rylab :)</div>
+      <div id="content">
+        <p className="large light">
+          someday&nbsp;I will&nbsp;make;
+          a&nbsp;cooler personal&nbsp;site;
+          it&nbsp;may be&nbsp;today
+        </p>
+        <p className="small dark hoverlight">
+          perhaps tomorrow;
+          but&nbsp;probably not&nbsp;either;
+          our&nbsp;planet still&nbsp;spins
+        </p>
+        <div id="digitalhaiku">#digitalhaiku</div>
+        <br />
+        {links.length && (
+          <>
+            <h1>Digital Selves</h1>
+            <ul className="narrow list">
+              {links.map(link => (
+                <li className={`link ${link.alias}`}>
+                  <a title={link.title} href={link.url} target="_blank">
+                    {link.name}</a>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
+      <a id="aeq" href="https://algorithmeq.com" target="_aeq">
+        <img height="77" width="77" className="bsd" src="/img/bsd_extrovert.png" alt="// MacOS <== ++BSD;" /></a>
     </Layout>
   )
 }
