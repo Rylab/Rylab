3796fcb45ada59abc99622be8c61b41dbdf43139
Ryan LaBarre <ryan@urbansitter.com>
2021-04-29 14:02:40 -0700

    update to next 10.2 and yarn upgrade; suppress poweredBy header; add simple custom 404 and 500 screens

---
+++ pages/404.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout, { siteTitle } from '../components/layout'
+
+export default function NotFound() {
+  return (
+    <Layout NotFound>
+      <Head>
+        <title>{siteTitle} :: 404 Page Not Found</title>
+      </Head>
+      <div className="content">
+        <Link href={'/'}>
+          <img
+            className="crosshair"
+            style={{ marginTop: 100, marginBottom: 50, height: 250, width: 250 }}
+            src="/img/bsd_extrovert.png" alt="RyLaB Home" />
+        </Link>
+        <p style={{ color: '#666', marginBottom: 250 }}>
+          <b>&lt;404&gt;</b>&nbsp;Not Found&nbsp;<b>&lt;/404&gt;</b>
+        </p>
+      </div>
+    </Layout>
+  )
+}
---
+++ pages/500.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout, { siteTitle } from '../components/layout'
+
+export default function InternalServerError() {
+  return (
+    <Layout InternalServerError>
+      <Head>
+        <title>{siteTitle} :: 500 Internal Server Error</title>
+      </Head>
+      <div className="content">
+        <Link href={'/'}>
+          <img
+            className="crosshair"
+            style={{ marginTop: 100, marginBottom: 50, height: 250, width: 250 }}
+            src="/img/bsd_extrovert.png" alt="RyLaB Home" />
+        </Link>
+        <p style={{ color: '#666', marginBottom: 250 }}>
+          <b>&lt;500&gt;</b>&nbsp;Internal Server Error&nbsp;<b>&lt;/500&gt;</b>
+        </p>
+      </div>
+    </Layout>
+  )
+}
