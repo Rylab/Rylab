f1e173ec5b0abe561dcba3f37bffeec4704a4370
Ryan LaBarre <ryan@urbansitter.com>
2023-02-24 07:40:27 -0800

    Swich to native fetch rather than OpenAI -> Axios library, to support Vercel edge functions (longer running time allowed -- maybe can add back biography calculation? Let's try again.

--- pages/api/tapeai/index.js
+++ pages/api/tapeai/index.js
@@ -1,6 +1,4 @@
-import { Configuration, OpenAIApi } from 'openai'
 import { validateUuid } from '../../../utils/helpers'
-import { dbCollection } from '../../../utils/mongodb'
 
 const { MANAGE_PASS } = process.env
 
@@ -35,142 +33,138 @@ const defaultGenres = [
   'rock',
 ]
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_KEY,
-  organization: process.env.OPENAI_ORG,
-})
 
-const jsonCoercion = ` Respond only with an array of 3 valid JSON objects with artist and title properties. `
-const jsonCoercionWithBio = ` Respond only with an array of 3 valid JSON objects, each having artist, title, and biography properties. `
+// const jsonCoercionWithBio = ` Respond only with an array of 3 valid JSON objects, each having artist, title, and biography properties. `
+const jsonCoercion = ` Respond only with an array of 3 valid JSON objects with artist, title, and biography properties. `
+  + `JSON response format: `
+  + `[{"title":"string","artist":"string","bio":string"},{"title":"string","artist":"string","bio":string"},{"title":"string","artist":"string","bio":string"}]`
 
-const getAdjectives = query => {
-  const trimmedAdjectives = query.adjectives.trim().replace(/^"(.+(?="$))"$/, '$1')
+
+const getAdjectives = adjectives => {
+  const trimmedAdjectives = adjectives.trim().replace(/^"(.+(?="$))"$/, '$1')
 
   return trimmedAdjectives ?? defaultAdjectives[~~(Math.random() * defaultAdjectives.length)]
 }
 
-const getGenre = query => {
-  const trimmedGenre = query.genre.trim().replace(/^"(.+(?="$))"$/, '$1')
+const getGenre = genre => {
+  const trimmedGenre = genre.trim().replace(/^"(.+(?="$))"$/, '$1')
 
   return trimmedGenre ?? defaultGenres[~~(Math.random() * defaultGenres.length)]
 }
 
-const getModel = query => {
-  return query.model?.trim() ?? defaultModel
+const getModel = model => {
+  if (model && model.trim()) return model.trim()
+
+  return defaultModel
 }
 
-const getTemperature = query => {
-  return query.temperature?.trim() ?? defaultTemperature
+const getTemperature = temperature => {
+  if (temperature && temperature.trim()) return temperature.trim() ?? defaultTemperature
+
+  return defaultTemperature
 }
 
-export default async function handler(req, res) {
-  if (!configuration.apiKey) {
-    res.status(500).json({
+export const config = {
+  runtime: 'edge',
+}
+
+export default async function handler(req) {
+  if (!process.env.OPENAI_KEY) {
+    return new Response(JSON.stringify({
       error: {
         message: 'OpenAI API key not set; see instructions in README.md',
       }
-    })
-    
-    return
+    }), { status: 500 })
   }
-  
-  const { body, headers, method, query } = req
 
+  const { headers, method, nextUrl } = req
+  const uuidHeader = headers.get('x-uuid')
   let uuid
-
-  if (headers['x-uuid'] && validateUuid(headers['x-uuid'])) {
-    uuid = headers['x-uuid']
+  
+  if (uuidHeader && validateUuid(uuidHeader)) {
+    uuid = uuidHeader
   } else {
     console.warn('Invalid or empty UUID:')
-    console.warn(uuid)
+    console.warn(uuidHeader)
   }
-
+  
   if (!uuid) {
-    res.status(400).json({
+    return new Response(JSON.stringify({
       error: {
         message: 'UUID not set; see instructions in README.md',
       }
-    })
-    
-    return
+    }), { status: 400 })
   }
   
-  let isAdmin = headers['x-admin'] === MANAGE_PASS
-  
+  const isAdmin = headers.get('x-admin') === MANAGE_PASS
+  const searchParams = new URLSearchParams(decodeURI(nextUrl.search))
+
   switch (method) {
     case 'GET':
-    const adjectives = getAdjectives(query)
-    const genre = getGenre(query)
+    const adjectives = getAdjectives(searchParams.get('adjectives'))
+    const genre = getGenre(searchParams.get('genre'))
 
     if (!genre) {
-      res.status(400).json({
+      return new Response(JSON.parse({
         error: {
           message: 'Genre type not valid; see instructions in README.md',
         }
-      })
-
-      return
+      }), { status: 400 })
     }
 
-    const openai = new OpenAIApi(configuration)
 
-    const model = getModel(req)
-    const temperature = getTemperature(req)
+    const model = getModel(searchParams.get('model'))
+    const temperature = getTemperature(searchParams.get('temperature'))
 
-    const cassettePrompt = generateCassettePrompt(genre, adjectives)
+    const prompt = generateCassettePrompt(genre, adjectives)
+
+    const completionRequest = {
+      max_tokens,
+      model,
+      prompt,
+      temperature,
+      user: uuid,
+    }
 
     try {
-      const cassetteRes = await openai.createCompletion({
-        max_tokens,
-        model,
-        prompt: cassettePrompt,
-        temperature,
-        user: uuid,
+      const cassettesResult = await fetch('https://api.openai.com/v1/completions', {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.OPENAI_KEY}`,
+          'OpenAI-Organization': process.env.OPENAI_ORG ?? '',
+        },
+        method: 'POST',
+        body: JSON.stringify(completionRequest),
       })
 
-      const cassetteJson = JSON.parse(cassetteRes.data.choices[0].text)
+      const cassettesJson = await cassettesResult.json()
+
       console.log(`\nGenre: ${genre}`)
       console.log(`Adjectives: ${adjectives}`)
-      console.log(cassetteJson)
+      console.log(cassettesJson.choices[0].text.trim())
 
-      res.status(200).json(cassetteJson)
+      return new Response(cassettesJson.choices[0].text.trim(), { status: 200 })
     } catch(error) {
       console.error(error)
 
       if (error.response) {
-        res.status(error.response.status).json(error.response.data)
+        return new Response(JSON.stringify(error.response), { status: 400 })
       } else {
-        res.status(500).json({
+        return new Response(JSON.stringify({
           error: {
             message: 'An unexpected error occurred processing your request.',
           }
-        })
+        }), { status: 400 })
       }
-    }
-  break
-
-  case 'POST':
-    try {
-      const { tapeAiCollection } = await dbCollection('tapeAi')
-      const { headers, body } = req
-
-      if (!isAdmin && headers['x-uuid'] !== body.uuid) {
-        res.status(401).json({ success: false })
-        break
-      }
-
-      const result = await tapeAiCollection.save(body)
-
-      res.status(200).json({ success: true, data: result })
-    } catch (error) {
-      console.error(error)
-      res.status(400).json({ success: false })
     }
   break
 
   default:
-    console.error(`Unexpected ${method} attempt on /api/songs`)
-    res.status(400).json({ success: false })
+    return new Response(JSON.stringify({
+      error: {
+        message: `Unexpected ${method} attempt on /api/songs`,
+      }
+    }), { status: 400 })
   break
   }
 }
