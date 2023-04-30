22800dfe810d0fab07d686ff6612b5040c756374
Ryan LaBarre <github@rylab.com>
2023-04-30 02:05:55 -0700

    Remove failing 14.x test build; add a couple sanity check index failure tests
    Also added geoip package in prep for upcoming feature test.

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,9 +1,11 @@
 import { render, screen } from '@testing-library/react'
 import { AppContext } from '../pages/_app'
 import Index from '../pages/index'
+import NotFound from '../pages/404'
+import InternalServerError from '../pages/500'
 
 describe('Index', () => {
-  it('renders a heading', () => {
+  it('renders main heading', () => {
     render(
       <AppContext.Provider value={{
         password: '',
@@ -16,6 +18,30 @@ describe('Index', () => {
     )
 
     const helloLine = screen.getByText('(: hello :)')
+
+    expect(helloLine).toBeInTheDocument()
+  })
+})
+
+describe('NotFound', () => {
+  it('renders 404 heading', () => {
+    render(
+      <NotFound />
+    )
+
+    const helloLine = screen.getByText('Not Found')
+
+    expect(helloLine).toBeInTheDocument()
+  })
+})
+
+describe('InternalServerError', () => {
+  it('renders 500 heading', () => {
+    render(
+      <InternalServerError />
+    )
+
+    const helloLine = screen.getByText('Internal Server Error')
 
     expect(helloLine).toBeInTheDocument()
   })
