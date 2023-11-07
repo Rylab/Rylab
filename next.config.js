d0ee3a35eca45adcfb8b52039744313b0fd4472a
Ryan LaBarre <github@rylab.com>
2023-11-07 09:54:17 -0800

    next 14 upgrade, other minor version bumps

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  experimental: { appDir: true },
-  poweredByHeader: false,
-  webpack(config) {
-    config.experiments = { ...config.experiments, topLevelAwait: true }
-    return config
-  },
+  poweredByHeader: false
 }
